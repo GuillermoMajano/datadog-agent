e6bef44c8c55da86ed20d0d2e6bee733e344a3ef
Pawel Chojnacki <pawelchcki@gmail.com>
2023-03-10 20:49:29 +0100

    w

--- test/benchmarks/apm_scripts/k6/02_run.sh
+++ test/benchmarks/apm_scripts/k6/02_run.sh
@@ -1,5 +1,8 @@
+#!/bin/bash -xe
 
-ARTIFACTS_DIR="/artifacts/${CI_JOB_ID}"
+WORKDIR="$LOCAL_TEST_PREFIX/benchmark_runner"
+
+ARTIFACTS_DIR="$LOCAL_TEST_PREFIX/artifacts/${CI_JOB_ID}"
 mkdir -p "${ARTIFACTS_DIR}"
 
 export DD_API_KEY=deadbeeffacefeeddeadbeeffacefeed
@@ -9,23 +12,19 @@ export DD_APM_MAX_CPU_PERCENT=200
 export DD_APM_REMOTE_TAGGER=false
 export K6_STATSD_ENABLE_TAGS=true
 export K6_STATSD_ADDR=$STATSD_URL
-mkdir runner
-export | grep PATH
 # fixup 
-apt-get install psmisc -y
-
-# store the test to use between runs between different branches
-cp ./test/benchmarks/apm_scripts/k6_basic.js ./runner/
+# apt-get install psmisc -y
 
 go run ./cmd/trace-agent &
+
 RUN_ID=k6-benchmark-dd-trace-agent
 k6 run \
     --no-usage-report \
     --tag test_run_id=$RUN_ID \
     --tag ci_job_id=$CI_JOB_ID \
     --out statsd \
-    --out json="reports/$RUN_ID.json.txt" \
-    ./runner/k6_basic.js
+    --out json="$ARTIFACTS_DIR/$RUN_ID.json" \
+    $WORKDIR/k6/basic.js
 
 killall -9 trace-agent
 
@@ -37,9 +36,7 @@ k6 run \
     --tag test_run_id=$RUN_ID \
     --tag ci_job_id=$CI_JOB_ID \
     --out statsd \
-    --out json="reports/$RUN_ID.json.txt" \
-    ./runner/k6_basic.js
+    --out json="$ARTIFACTS_DIR/$RUN_ID.json" \
+    $WORKDIR/k6/basic.js
 
 killall -9 trace-agent
-
-git checkout "${CI_COMMIT_REF_NAME}" # (Only needed while these changes aren't merged to main)
